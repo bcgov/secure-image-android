7b59c418160d3b22338d94c4de6a5e8e6f1ab8a9
Aidan Laing <aidan@freshworks.io>
2017-12-31 13:17:57 -0800

    Realm Encryption Key Refactor
    Moved encryption key from baes application to a KeyStorageService class

--- app/src/main/java/ca/bc/gov/secureimage/common/base/BaseApplication.kt
+++ app/src/main/java/ca/bc/gov/secureimage/common/base/BaseApplication.kt
@@ -8,13 +8,7 @@ import android.preference.PreferenceManager
 import ca.bc.gov.secureimage.BuildConfig
 import com.squareup.leakcanary.LeakCanary
 import java.security.*
-import android.security.keystore.KeyProperties
-import android.security.keystore.KeyGenParameterSpec
-import android.util.Base64
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
-import javax.crypto.Cipher
-import javax.crypto.spec.GCMParameterSpec
+import ca.bc.gov.secureimage.common.services.KeyStorageService
 
 /**
  * Created by Aidan Laing on 2017-12-12.
@@ -25,96 +19,27 @@ class BaseApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        setUpRealm()
+        setUpRealm(KeyStorageService(KeyStore.getInstance("AndroidKeyStore")))
         setUpStrictMode()
         setUpLeakCanary()
     }
 
-    private fun setUpRealm() {
+    private fun setUpRealm(keyStorageService: KeyStorageService) {
 
         Realm.init(this)
 
+        val key = keyStorageService.getSecureKey(
+                "realm",
+                64,
+                PreferenceManager.getDefaultSharedPreferences(this))
+
         val config = RealmConfiguration.Builder()
                 .deleteRealmIfMigrationNeeded()
-                .encryptionKey(loadKey("realm"))
+                .encryptionKey(key)
                 .build()
 
         Realm.setDefaultConfiguration(config)
 
-    }
-
-    private fun loadKey(
-            alias: String,
-            realmKeySize: Int = 64,
-            keyStoreKeySize: Int = 256,
-            keyStoreType: String = "AndroidKeyStore",
-            transformation: String = "AES/GCM/NoPadding",
-            tLen: Int = 128,
-            ivKey: String = "ivKey"
-    ): ByteArray {
-
-        // Key store setup
-        val keystore = KeyStore.getInstance(keyStoreType)
-        keystore.load(null)
-
-        // Generating AES key if alias is not found
-        if (!keystore.containsAlias(alias)) {
-            val gen = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, keyStoreType)
-
-            gen.init(KeyGenParameterSpec.Builder(
-                    alias, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
-                    .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                    .setKeySize(keyStoreKeySize)
-                    .build())
-
-            gen.generateKey()
-        }
-
-        // Grabbing secret key from keystore
-        val secretKey = keystore.getKey(alias, null) as SecretKey
-
-        // Shared Prefs setup
-        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
-
-        // If shared prefs doesn't have saved key generate random, encrypt, and save to shared prefs
-        if(!sharedPrefs.contains(alias)) {
-
-            // Encryption setup
-            val encryptionCipher = Cipher.getInstance(transformation)
-            encryptionCipher.init(Cipher.ENCRYPT_MODE, secretKey)
-
-            // Random key
-            val randomKey = ByteArray(realmKeySize)
-            SecureRandom().nextBytes(randomKey)
-
-            // Encrypt random key
-            val encryptedBytes = encryptionCipher.doFinal(randomKey)
-
-            // Saving base 64 encoded encrypted bytes
-            val base64EncryptedBytesString = Base64.encodeToString(encryptedBytes, Base64.DEFAULT)
-            sharedPrefs.edit().putString(alias, base64EncryptedBytesString).apply()
-
-            // Saving base 64 encoded initialization vector
-            val base64IvString = Base64.encodeToString(encryptionCipher.iv, Base64.DEFAULT)
-            sharedPrefs.edit().putString(ivKey, base64IvString).apply()
-        }
-
-        // Decoding saved encrypted bytes
-        val base64EncryptedBytesString = sharedPrefs.getString(alias, "")
-        val encryptedBytes = Base64.decode(base64EncryptedBytesString, Base64.DEFAULT)
-
-        // Decoding saved initialization vector
-        val base64IvString = sharedPrefs.getString(ivKey, "")
-        val savedIv = Base64.decode(base64IvString, Base64.DEFAULT)
-
-        // Decrypting setup
-        val decryptionCipher = Cipher.getInstance(transformation)
-        val gcmParameterSpec = GCMParameterSpec(tLen, savedIv)
-        decryptionCipher.init(Cipher.DECRYPT_MODE, secretKey, gcmParameterSpec)
-
-        // Decrypting encrypted bytes
-        return decryptionCipher.doFinal(encryptedBytes)
     }
 
     // Strict mode
---
+++ app/src/main/java/ca/bc/gov/secureimage/common/services/KeyStorageService.kt
@@ -0,0 +1,124 @@
+package ca.bc.gov.secureimage.common.services
+
+import android.content.SharedPreferences
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.util.Base64
+import java.security.KeyStore
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
+
+/**
+ * Created by Aidan Laing on 2017-12-31.
+ *
+ */
+class KeyStorageService(private val keyStore: KeyStore) {
+
+    init {
+        keyStore.load(null)
+    }
+
+    /**
+     * Gets secure key using AES encryption
+     */
+    fun getSecureKey(
+            alias: String,
+            keySize: Int,
+            sharedPrefs: SharedPreferences
+    ): ByteArray {
+
+        // Gets AES secret key from key store
+        val aesSecretKey = getAESSecretKey(alias, 256)
+
+        // Creates new random key and encrypts it using the AES secret key
+        if(!sharedPrefs.contains(alias)) {
+            generateRandomAESEncryptedKey(alias, keySize, aesSecretKey, sharedPrefs)
+        }
+
+        // Gets stored key and decrypts it using the AES secret key
+        return getDecryptedKey(alias, aesSecretKey, sharedPrefs)
+    }
+
+    private fun getAESSecretKey(
+            alias: String,
+            keySize: Int
+    ): SecretKey {
+
+        // Generating AES key if alias is not found
+        if (!keyStore.containsAlias(alias)) {
+            generateAESSecretKey(alias, keySize)
+        }
+
+        // Getting secret key from keystore and returning
+        return keyStore.getKey(alias, null) as SecretKey
+    }
+
+    private fun generateAESSecretKey(alias: String, keySize: Int) {
+        val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, keyStore.provider)
+
+        keyGenerator.init(KeyGenParameterSpec.Builder(
+                alias, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .setKeySize(keySize)
+                .build())
+
+        keyGenerator.generateKey()
+    }
+
+    private fun generateRandomAESEncryptedKey(
+            alias: String,
+            keySize: Int,
+            aesSecretKey: SecretKey,
+            sharedPrefs: SharedPreferences) {
+
+        // Encryption setup
+        val encryptionCipher = Cipher.getInstance(AES_TRANSFORMATION)
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, aesSecretKey)
+
+        // New random encrypted key
+        val randomKey = ByteArray(keySize)
+        SecureRandom().nextBytes(randomKey)
+        val encryptedBytes = encryptionCipher.doFinal(randomKey)
+
+        // Saving base 64 encoded encrypted bytes
+        val base64EncryptedBytesString = Base64.encodeToString(encryptedBytes, Base64.DEFAULT)
+        sharedPrefs.edit().putString(alias, base64EncryptedBytesString).apply()
+
+        // Saving base 64 encoded initialization vector
+        val base64IvString = Base64.encodeToString(encryptionCipher.iv, Base64.DEFAULT)
+        sharedPrefs.edit().putString(IV_KEY, base64IvString).apply()
+    }
+
+    private fun getDecryptedKey(
+            alias: String,
+            aesSecretKey: SecretKey,
+            sharedPrefs: SharedPreferences
+    ): ByteArray {
+
+        // Decoding saved encrypted bytes
+        val base64EncryptedBytesString = sharedPrefs.getString(alias, "")
+        val encryptedBytes = Base64.decode(base64EncryptedBytesString, Base64.DEFAULT)
+
+        // Decoding saved initialization vector
+        val base64IvString = sharedPrefs.getString(IV_KEY, "")
+        val savedIv = Base64.decode(base64IvString, Base64.DEFAULT)
+
+        // Decryption setup
+        val decryptionCipher = Cipher.getInstance(AES_TRANSFORMATION)
+        val gcmParameterSpec = GCMParameterSpec(128, savedIv)
+        decryptionCipher.init(Cipher.DECRYPT_MODE, aesSecretKey, gcmParameterSpec)
+
+        // Decrypt encrypted bytes and return
+        return decryptionCipher.doFinal(encryptedBytes)
+    }
+
+    companion object {
+        private val AES_TRANSFORMATION = "AES/GCM/NoPadding"
+        private val IV_KEY = "IV_KEY"
+    }
+
+}
